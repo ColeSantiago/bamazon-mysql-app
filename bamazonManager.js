a6fa31bb576610f1f9c00e2c2a41ce6a9b2ce9e6
Cole Santiago <colesantiago@Coles-MBP.fios-router.home>
2018-02-19 19:51:54 -0500

    third option function is working

--- bamazonManager.js
+++ bamazonManager.js
@@ -34,7 +34,7 @@ function displayManagerOptions() {
 				viewLowInv();
 
 			} if (managerChoice.choice === 'Add to Inventory') {
-				console.log('option Add to Inventory');
+				addInv();
 
 			} if (managerChoice.choice === 'Add New Product') {
 				console.log('option Add New Product');
@@ -76,5 +76,69 @@ function viewLowInv() {
 		console.log('^If low inventory levels exist they will be listed above^')
 		displayManagerOptions();
 	})
+};
+
+function addInv() {
+		connection.query("SELECT * FROM products", function(err, results) {
+		if (err) throw err;
+
+		inquirer.prompt([
+			{
+				name: 'productId',
+				type: 'input',
+				message: 'What is the product ID of the item you would like to add inventory to?',
+				validate: function(value) {
+			          if (isNaN(value) === false && value > 0 && value < 11 && value % 1 === 0) {
+			            return true;
+			          }
+			          console.log('Please input a valid number.');
+			          return false;
+	        	}
+			},
+			{
+				name: 'quantity',
+				type: 'input',
+				message: 'How many items would you like to add?',
+				validate: function(value) {
+			          if (isNaN(value) === false && value % 1 === 0) {
+			            return true;
+			          }
+			          console.log('Please input a valid number.');
+			          return false;
+	        	}
+			}
+		])
+		.then(function(answer) {
+			let chosenId = parseInt(answer.productId);
+			let chosenQuantity = parseInt(answer.quantity);
+			let chosenProduct;
+			for(let i = 0; i < results.length; i++) {
+				if (results[i].item_id === chosenId) {
+					chosenProduct = results[i];
+				}
+			}
+			let quantity = chosenQuantity + chosenProduct.stock_quantity
+			updateInv(quantity, chosenProduct.item_id);
+			console.log('Inventory Updated!');
+			displayManagerOptions();
+		});
+	});
+};
+
+function updateInv(quantity, item_id) {
+		connection.query(
+		'UPDATE products SET ? WHERE ?',
+		[
+			{
+				stock_quantity: quantity
+			},
+			{
+				item_id: item_id
+			}
+		],
+		function(err, results) {
+			if (err) throw err;
+		}
+	)
 };
 
